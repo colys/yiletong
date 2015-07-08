cbf867782b047c6e103c2a1478c341f8f1e46ddd
tlo.colys@gmail.com <tlo.colys@gmail.com>
2015-07-09 02:40:52 +0800

    init

---
+++ WinFormDemo/frmMain.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace WinFormDemo
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.测试ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.跳转到ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.触发查询ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.载入ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.系统ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.添加客户ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.客户列表ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.chromeWebBrowser1 = new Sashulin.ChromeWebBrowser();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.测试ToolStripMenuItem,
+            this.文件ToolStripMenuItem,
+            this.系统ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1175, 32);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 测试ToolStripMenuItem
+            // 
+            this.测试ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.跳转到ToolStripMenuItem,
+            this.触发查询ToolStripMenuItem,
+            this.testToolStripMenuItem});
+            this.测试ToolStripMenuItem.Name = "测试ToolStripMenuItem";
+            this.测试ToolStripMenuItem.Size = new System.Drawing.Size(58, 28);
+            this.测试ToolStripMenuItem.Text = "测试";
+            // 
+            // 跳转到ToolStripMenuItem
+            // 
+            this.跳转到ToolStripMenuItem.Name = "跳转到ToolStripMenuItem";
+            this.跳转到ToolStripMenuItem.Size = new System.Drawing.Size(198, 28);
+            this.跳转到ToolStripMenuItem.Text = "跳转到";
+            this.跳转到ToolStripMenuItem.Click += new System.EventHandler(this.跳转到ToolStripMenuItem_Click);
+            // 
+            // 触发查询ToolStripMenuItem
+            // 
+            this.触发查询ToolStripMenuItem.Name = "触发查询ToolStripMenuItem";
+            this.触发查询ToolStripMenuItem.Size = new System.Drawing.Size(198, 28);
+            this.触发查询ToolStripMenuItem.Text = "开始监控";
+            this.触发查询ToolStripMenuItem.Click += new System.EventHandler(this.触发查询ToolStripMenuItem_Click);
+            // 
+            // testToolStripMenuItem
+            // 
+            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(198, 28);
+            this.testToolStripMenuItem.Text = "test";
+            this.testToolStripMenuItem.Click += new System.EventHandler(this.testToolStripMenuItem_Click);
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.载入ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(58, 28);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 载入ToolStripMenuItem
+            // 
+            this.载入ToolStripMenuItem.Name = "载入ToolStripMenuItem";
+            this.载入ToolStripMenuItem.Size = new System.Drawing.Size(198, 28);
+            this.载入ToolStripMenuItem.Text = "载入";
+            this.载入ToolStripMenuItem.Click += new System.EventHandler(this.载入ToolStripMenuItem_Click);
+            // 
+            // 系统ToolStripMenuItem
+            // 
+            this.系统ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.添加客户ToolStripMenuItem,
+            this.客户列表ToolStripMenuItem});
+            this.系统ToolStripMenuItem.Name = "系统ToolStripMenuItem";
+            this.系统ToolStripMenuItem.Size = new System.Drawing.Size(58, 28);
+            this.系统ToolStripMenuItem.Text = "系统";
+            // 
+            // 添加客户ToolStripMenuItem
+            // 
+            this.添加客户ToolStripMenuItem.Name = "添加客户ToolStripMenuItem";
+            this.添加客户ToolStripMenuItem.Size = new System.Drawing.Size(198, 28);
+            this.添加客户ToolStripMenuItem.Text = "添加客户";
+            // 
+            // 客户列表ToolStripMenuItem
+            // 
+            this.客户列表ToolStripMenuItem.Name = "客户列表ToolStripMenuItem";
+            this.客户列表ToolStripMenuItem.Size = new System.Drawing.Size(198, 28);
+            this.客户列表ToolStripMenuItem.Text = "客户列表";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 32);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1175, 560);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.chromeWebBrowser1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1167, 527);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.webBrowser1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1167, 527);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // chromeWebBrowser1
+            // 
+            this.chromeWebBrowser1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.chromeWebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chromeWebBrowser1.Location = new System.Drawing.Point(3, 3);
+            this.chromeWebBrowser1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.chromeWebBrowser1.Name = "chromeWebBrowser1";
+            this.chromeWebBrowser1.Size = new System.Drawing.Size(1161, 521);
+            this.chromeWebBrowser1.TabIndex = 1;
+            this.chromeWebBrowser1.BrowserCreated += new System.EventHandler(this.chromeWebBrowser1_BrowserCreated);
+            this.chromeWebBrowser1.BrowserNavigated += new System.EventHandler(this.chromeWebBrowser1_BrowserNavigated);
+            this.chromeWebBrowser1.BrowserDocumentCompleted += new System.EventHandler(this.chromeWebBrowser1_BrowserDocumentCompleted);
+            this.chromeWebBrowser1.BrowserFrameLoadEnd += new System.EventHandler(this.chromeWebBrowser1_BrowserFrameLoadEnd);
+            this.chromeWebBrowser1.PageLoadFinishEventhandler += new System.EventHandler(this.chromeWebBrowser1_PageLoadFinishEventhandler);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1161, 521);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1175, 592);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "frmMain";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 测试ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 跳转到ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 载入ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 触发查询ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 系统ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 添加客户ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 客户列表ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private Sashulin.ChromeWebBrowser chromeWebBrowser1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ WinFormDemo/frmMain.cs
@@ -0,0 +1,256 @@
+﻿using Sashulin;
+using Sashulin.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormDemo
+{
+
+    [System.Security.Permissions.PermissionSet(System.Security.Permissions.SecurityAction.Demand, Name = "FullTrust")]
+    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
+    public partial class frmMain : Form
+    {
+        string userName="福州易乐通";
+        string password = "Ylt123456";
+        Queue<string> terminalQueue = new Queue<string>();
+        public frmMain()
+        {
+            InitializeComponent();
+            CSharpBrowserSettings settings = new CSharpBrowserSettings();
+            settings.DefaultUrl = "www.baidu.com";
+            //settings.UserAgent = "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 4 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19";
+            settings.CachePath = @"C:\temp\caches";
+            chromeWebBrowser1.Initialize(settings);
+            webBrowser1.Url =new Uri( Application.StartupPath + "\\main.html");
+            webBrowser1.ObjectForScripting = this;
+            
+        }
+
+        private string getNextTerminal()
+        {
+            if (terminalQueue.Count == 0) { FillQueue(); }
+            return terminalQueue.Dequeue();
+        }
+
+        private void FillQueue()
+        {
+            terminalQueue.Enqueue("65886057");
+            terminalQueue.Enqueue("65886058");
+            terminalQueue.Enqueue("65886059");
+            terminalQueue.Enqueue("65886060");
+            terminalQueue.Enqueue("65886061");
+            terminalQueue.Enqueue("65886062");
+        }
+
+        private void 载入ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            chromeWebBrowser1.OpenUrl("https://119.4.99.217:7300/mcrm/login.jsp");
+        }
+
+        private void 触发登录ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CwbElement buttons = chromeWebBrowser1.Document.GetElementById("submitButton");
+            buttons.Click();
+        }
+
+        private void 跳转到ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            loadFrame();
+            
+        }
+
+        private void loadFrame()
+        {
+              CwbElement leftMenuUl = chromeWebBrowser1.Document.GetElementById("resTree");
+              if (leftMenuUl == null)
+              {
+                  MessageBox.Show("请先载入！");
+                  return;
+              }
+             foreach(CwbElement ele in leftMenuUl.ChildElements){
+                 if (ele.ChildElements[0].GetAttribute("node-id").Equals("8053"))
+                 {
+                     CwbElement subUl = ele.ChildElements[1];
+                     foreach (CwbElement subEle in subUl.ChildElements)
+                     {
+                         if (subEle.ChildElements[0].GetAttribute("node-id").Equals("10051"))
+                         {
+                             subEle.ChildElements[0].Click();  
+                         }
+                     }
+                 }
+             }
+        }
+
+        System.Threading.Thread waitQueryThread;
+        bool inQuery = false;
+
+        private void 触发查询ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            object frameUrl = chromeWebBrowser1.EvaluateScript("window.frames['收单日志']==null");
+            if (frameUrl == null || frameUrl.Equals(true))
+            {
+                MessageBox.Show("请先跳转到收单日志");
+                return;
+            }
+            timer1.Start();
+            GoQuery();
+            tabControl1.SelectedIndex = 1;
+            触发查询ToolStripMenuItem.Enabled = false;
+        }
+        string termID,beginDate,endDate;
+        private void GoQuery()
+        {
+            if (waitQueryThread != null && waitQueryThread.IsAlive) return;
+            if (inQuery) return;
+            inQuery = true;
+            //CwbElement tabFrame = chromeWebBrowser1.Document.GetElementById("tab1");
+            //if(tabFrame==null) return;
+            object notFunction = chromeWebBrowser1.EvaluateScript("typeof( window.frames['收单日志'].queryByCondition) == 'undefined'");
+            if (notFunction != null && notFunction.Equals(true))
+            {
+                MessageBox.Show("完蛋了，查询方法被修改了！");
+            }
+            else
+            {
+
+                termID = getNextTerminal();
+                object date1 = chromeWebBrowser1.EvaluateScript(" $(window.frames['收单日志'].document).find('#beginstdate').next().find('.combo-value').val()");
+                object date2 = chromeWebBrowser1.EvaluateScript(" $(window.frames['收单日志'].document).find('#endstdate').next().find('.combo-value').val()");
+                if (date1!=null) beginDate = date1.ToString();
+                else date1 = "找不到";
+                if (date2 != null) endDate = date2.ToString();
+                else endDate = "找不到";
+               string js=@"
+$(window.frames['收单日志'].document).find('#tid').val('" + termID + @"');
+$(window.frames['收单日志'].document).find('.pagination-page-list:eq(0)').find('option:last').attr('selected',true);
+window.frames['收单日志'].queryByCondition();";
+                chromeWebBrowser1.ExecuteScript(js);
+                //chromeWebBrowser1.ExecuteScript("");
+                setStatus("正在查询" + termID + "的刷卡情况");
+                waitQueryThread = new System.Threading.Thread(new System.Threading.ParameterizedThreadStart(watinQuery));
+                waitQueryThread.Start();
+            }
+        }
+
+        private void setStatus(string str)
+        {
+            object[] objects = new object[1];
+            objects[0] =str;
+            webBrowser1.Document.InvokeScript("setStatus", objects);
+        }
+
+        public void OnQueryFinish()
+        {
+            setStatus( termID+"终端"+beginDate+"到"+ endDate + "的刷卡情况:");
+            object table= chromeWebBrowser1.EvaluateScript("$(window.frames['收单日志'].document).find('.datagrid-btable:eq(1)')[0].outerHTML");            
+            HtmlElement hidden_div = webBrowser1.Document.GetElementById("hidden_div");
+            hidden_div.InnerHtml = table.ToString();
+            object[] objects = new object[1];             
+            object jsonStr = webBrowser1.Document.InvokeScript("parseTableJson", objects);
+//            if(jsonStr!=null) MessageBox.Show(jsonStr.ToString());
+            inQuery = false;
+        }
+
+        public delegate void QueryFinish();
+        private void watinQuery(object o)
+        {
+            while (true)
+            {
+               object isViable= chromeWebBrowser1.EvaluateScript("$(window.frames['收单日志'].document).find('.datagrid-mask-msg').is(':visible')");
+               if (isViable != null && isViable.Equals(false))
+               {
+                   this.BeginInvoke(new QueryFinish(OnQueryFinish));
+                   return;
+               }
+               else System.Threading.Thread.Sleep(100);
+            }
+        }
+
+        private void chromeWebBrowser1_PageLoadFinishEventhandler(object sender, EventArgs e)
+        {
+            //if (chromeWebBrowser1.Document == null) return;
+            //int pos = chromeWebBrowser1.Url.LastIndexOf("/");
+            //string pageName = chromeWebBrowser1.Url.Substring(pos + 1).ToLower();
+            //switch (pageName)
+            //{
+            //    case "login.jsp":
+            //        //还要有登录失败的判断
+            //        goLogin();
+            //        break;
+            //}
+           // MessageBox.Show(pageName);
+        }
+
+        private void goLogin()
+        {
+            //if (chromeWebBrowser1.Document == null) return;
+            //CwbElement userNameEle = chromeWebBrowser1.Document.GetElementById("username");
+            //if (userNameEle!=null) userNameEle.Value = userName;
+
+            //CwbElement passwordEle = chromeWebBrowser1.Document.GetElementById("password");
+            //if(passwordEle!=null)passwordEle.Value = password;
+            chromeWebBrowser1.ExecuteScript("$('#username').val('"+userName+"')");
+            chromeWebBrowser1.ExecuteScript("$('#password').val('" + password + "')");
+            chromeWebBrowser1.ExecuteScript("$('input[name=codeVal]').focus()");
+             
+        }
+
+        private void chromeWebBrowser1_BrowserNavigated(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void chromeWebBrowser1_BrowserDocumentCompleted(object sender, EventArgs e)
+        {
+            if (chromeWebBrowser1.Document == null) return;
+            int pos = chromeWebBrowser1.Url.LastIndexOf("/");
+            string pageName = chromeWebBrowser1.Url.Substring(pos + 1).ToLower();
+            switch (pageName)
+            {
+                case "login.jsp":
+                case "j_spring_security_check":
+                    //还要有登录失败的判断
+                    goLogin();
+                    break;
+                case "index":
+                    string jsPath = "http://localhost:22163/scripts/main.js";//file:///" + Application.StartupPath.Replace("\\","/") + "/main.js";
+                    string jsScript = @"$('<div></div>').attr('id','hidden_div').appendTo(document.body)";
+                    //chromeWebBrowser1.ExecuteScript(" $(\"" + jsScript + "\")appendTo(document.body)");
+                    //loadFrame();
+                    chromeWebBrowser1.ExecuteScript(jsScript);
+                    break;
+
+            } 
+        }
+
+        private void chromeWebBrowser1_BrowserFrameLoadEnd(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void chromeWebBrowser1_BrowserCreated(object sender, EventArgs e)
+        {
+            chromeWebBrowser1.LoadHtml("点击菜单 文件->载入");
+        }
+
+        private void testToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            chromeWebBrowser1.ExecuteScript("test()");
+        }
+
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            if (!inQuery) GoQuery();
+        }
+
+     
+    }
+}
